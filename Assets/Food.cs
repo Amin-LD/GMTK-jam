62314b37eb68a3722c4f424ebd62da748a436fd0
monorush <mark.mpe@gmail.com>
2020-07-11 20:49:48 +0200

    Can now eat food and there is timer added

---
+++ Assets/Food.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public float timeToAdd = 10f;
+    public string foodName = "Apple";
+    [HideInInspector]
+    public bool canInteract;
+    private GameObject player;
+
+    private GameController gc;
+    private void Start()
+    {
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if(canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            gc.foodTimer += timeToAdd;
+            player.GetComponent<PlayerController>().triggerPanel.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canInteract = true;
+            player.GetComponent<PlayerController>().triggerPanel.transform.GetComponentInChildren<TextMeshProUGUI>().text = "Press 'E' to eat: " + foodName;
+            player.GetComponent<PlayerController>().triggerPanel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canInteract = false;
+            player.GetComponent<PlayerController>().triggerPanel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject foodBar;
+
+    public float maxTime = 100f;
+    public float foodTimer;
+
+    private void Awake()
+    {
+        foodTimer = maxTime;
+    }
+
+    private void Update()
+    {
+        UpdateGUI();
+    }
+
+    private void UpdateGUI()
+    {
+        foodTimer -= Time.deltaTime;
+        if (foodTimer > maxTime)
+            foodTimer = maxTime;
+        foodBar.transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = foodTimer.ToString("0.00");
+        foodBar.transform.GetChild(0).GetComponent<Slider>().value = foodTimer;
+    }
+}
